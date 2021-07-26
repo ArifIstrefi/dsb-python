f1a123ceb456ffacb2b6d875e7afe265be56c491
Arif Istrefi <arifi@kosbit.local>
2021-07-26 20:03:45 +0200

    Komiti i 2

--- hell.py
+++ hell.py
@@ -1,3 +1,12 @@
 print("Learning git and python")
 print("I am very excited.")
 print("my stduents will copyy everything I write")
+
+
+print("   *   ")
+print("  ***  ")
+print(" ----- ")
+print("-------")
+print("   -   ")
+print("   -   ")
+print("   -   ")
